9b34463f59842b948f76f53cc7e6bc7d6a87bc99
mattsgit <matthewradams22@gmail.com>
2017-11-24 01:18:14 -0600

    Added support for windows and linux. refs none

--- generate_anagrams.py
+++ generate_anagrams.py
@@ -1,24 +1,27 @@
-import string
+import os
 import random
+import string
 
 OTHER = '!@#      $%^&*()_          +=-[]{}     \|?>/.,<     `~'
-COUNTS = ['100', '1000', '10000', '100000']
 
 
 def id_generator(size, chars=string.ascii_uppercase + string.ascii_lowercase + string.digits + OTHER):
     return ''.join(random.choice(chars) for _ in range(size))
 
 
-def create_anagram_files():
-    for count in COUNTS:
-        with open(r'anagram_files\%s_anagrams.txt' % count, 'w') as the_file:
+def create_anagram_files(counts):
+    file_prefix = r'anagram_files/'
+    if os.name == 'nt':
+        file_prefix = r'anagram_files\\'
+    for count in counts:
+        with open(r'%s%s_anagrams.txt' % (file_prefix, count), 'w') as the_file:
             for i in xrange(1, int(count) + 1):
                 size = (random.randint(32, 64))
                 anagram = id_generator(size)
                 the_file.write(
                     '"%s","%s"\n' % (''.join(random.sample(anagram, size)), ''.join(random.sample(anagram, size))))
 
-        with open(r'anagram_files\%s_not_anagrams.txt' % count, 'w') as the_file:
+        with open(r'%s%s_not_anagrams.txt' % (file_prefix, count), 'w') as the_file:
             for i in xrange(1, int(count) + 1):
                 size = (random.randint(32, 64))
                 anagram = id_generator(size)
--- test_summary.py
+++ test_summary.py
@@ -6,17 +6,20 @@ import time
 
 
 def main():
-    anagram_tester = anagram_checker.AnagramTester()
+    counts = ['100', '1000', '10000', '100000']
     print 'Generating random anagram files. Each row has 2 strings of length 32-64 randomly sorted. ' \
           '"not" files have different character appended to each anagram to force false.'
     generate_starts = time.time()
-    generate_anagrams.create_anagram_files()
+    generate_anagrams.create_anagram_files(counts)
     print 'File generation took: %s seconds\n' % (time.time() - generate_starts)
     print 'Testing random files. Expect all True or False output for generated files and linear time and space complexity\n'
 
+    anagram_tester = anagram_checker.AnagramTester()
     for file in os.listdir("anagram_files"):
         if file.endswith(".txt"):
-            filename = 'anagram_files\%s' % file
+            filename = 'anagram_files/%s' % file
+            if os.name == 'nt':
+                filename = 'anagram_files\%s' % file
             tester_starts = time.time()
             results = anagram_tester.test_is_anagram_with_file(filename)
             print 'Results are: %s. Took total of %s seconds to load and process file %s' % (
